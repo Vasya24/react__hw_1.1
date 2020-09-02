43402ceb13e88212d3a9e495053a70b67fd531ac
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-09-02 11:11:28 +0300

    Done

---
+++ src/models/Item.js
@@ -0,0 +1,12 @@
+export class Item {
+    constructor(entity) {
+        Object.assign(this, {
+            brand: 'Brand',
+            title: 'Title',
+            description: 'Description',
+            descriptionFull: 'Full description',
+            price: 0,
+            currency: '£'
+        }, entity)
+    }
+}
